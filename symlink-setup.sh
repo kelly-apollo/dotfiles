d470b010139690da25bef51bd4349dc40e8d2709
Kelly <nagisa.fff@gmail.com>
2018-07-03 17:49:22 +0800

    Add hammerspoon

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -126,8 +126,7 @@ print_success() {
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
 FILES_TO_SYMLINK="$FILES_TO_SYMLINK bin .tmuxinator .proxychains"
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .config/fish/config.fish .config/fish/custom"
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .config/flake8"
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK .config .hammerspoon"
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
