6815d9c5b41227904af0ccdd710471f6cf470739
Kelly <nagisa.fff@gmail.com>
2018-07-04 23:55:11 +0800

    Add hammerspoon modifier key

---
+++ .hammerspoon/windows-bindings.lua
@@ -0,0 +1,23 @@
+return {
+  modifiers = {'ctrl'},
+  showHelp  = false,
+  trigger   = 's',
+  mappings  = {
+    { {},         'return', 'centerHalf' },
+    { {},         'tab',  'center' },
+    { {},         'space',  'maximize' },
+    { {},         'h',      'left' },
+    { {},         'j',      'down' },
+    { {},         'k',      'up' },
+    { {},         'l',      'right' },
+    { {'shift'},  'h',      'left40' },
+    { {'shift'},  'l',      'right60' },
+    { {},         'i',      'upLeft' },
+    { {},         'o',      'upRight' },
+    { {},         ',',      'downLeft' },
+    { {},         '.',      'downRight' },
+    { {},         'n',      'nextScreen' },
+    { {},         'right',  'moveOneScreenEast' },
+    { {},         'left',   'moveOneScreenWest' },
+  }
+}
