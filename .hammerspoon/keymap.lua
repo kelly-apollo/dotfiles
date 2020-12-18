daaf10a5d6f873e511694340015b6563a7740fc6
Kelly <nagisa.fff@gmail.com>
2020-12-18 15:13:11 +0800

    Fixed datetime input

--- .hammerspoon/keymap.lua
+++ .hammerspoon/keymap.lua
@@ -12,14 +12,21 @@ function stroke(modes, key)
         hs.eventtap.keyStroke(modes, key)
     end
 end
+
 function strokes(text)
     return function()
+        if type(text) == 'function' then
+            text = text()
+        end
         hs.eventtap.keyStrokes(text)
     end
 end
+
 function getToday()
     local date_table = os.date("*t")
     local year, month, day = date_table.year, date_table.month, date_table.day
+
+
     return string.format("%d-%d-%d", year, month, day)
 end
 
@@ -28,12 +35,10 @@ hs.hotkey.bind({}, 'F2', open('IntelliJ IDEA CE'))
 hs.hotkey.bind({}, 'F3', open('印象笔记'))
 hs.hotkey.bind({}, 'F4', open('iTerm'))
 hs.hotkey.bind({}, 'F5', open('Finder'))
-hs.hotkey.bind({}, 'F12', function()
-    hs.execute('pmset displaysleepnow')
-end)
+hs.hotkey.bind({}, 'F12', function() hs.execute('pmset displaysleepnow') end)
 
 hs.hotkey.bind({'alt'}, 'j', stroke({'ctrl', 'shift', 'alt'}, 'j'))
-hs.hotkey.bind({'alt'}, 'd', strokes(getToday()))
+hs.hotkey.bind({'alt'}, 'd', strokes(getToday))
 hs.hotkey.bind({'alt', 'shift'}, 'b', strokes('bioserenity'))
 hs.hotkey.bind({'alt', 'shift'}, 'r', strokes('referential'))
 hs.hotkey.bind({'alt', 'shift'}, 'c', strokes('cardioskin'))
